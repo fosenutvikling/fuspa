02993a58cd65a859176360c0b636b123b9a695c1
Jonas Kirkemyr <jonas@fosen-utvikling.no>
2016-08-23 23:53:25 +0200

    chore: update build process to webpack

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,10 @@
 module.exports = function (grunt) {
     'use strict';
 
+    var webpack = require('webpack');
     grunt.initConfig({
         pkg: grunt.file.readJSON('./package.json'),
-        
+
         //minify all js files
         uglify: {
             options: {
@@ -16,27 +17,53 @@ module.exports = function (grunt) {
             }
         },
 
-        typescript: {
-            base: {
-                src: ['src/**/*.ts'],
-                dest: 'build/spa.js',
-                options: {
-                    module: 'commonjs',
-                    target: 'es5',
-                    sourceMap: true,
-                    declaration: true,
-                    references: [
-                        "typings/main.d.ts"
+        webpack: {
+            web: {
+                entry: './src/main.ts',
+                output: {
+                    filename: './dist/spa.min.js',
+                    library: 'Spa',
+                    libraryTarget: 'umd',
+                    umdNamedDefine: true,
+                },
+                devtool: 'source-map',
+                resolve: {
+                    // Add `.ts` and `.tsx` as a resolvable extension.
+                    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+                },
+                module: {
+                    loaders: [
+                        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+                        { test: /\.tsx?$/, loader: 'ts-loader' }
                     ]
-                }
+                },
+                node: {
+                    fs: "empty"
+                },
+                plugins: [
+                    new webpack.optimize.UglifyJsPlugin()
+                ]
+            }
+        },
+
+        dtsGenerator: {
+            options: {
+                baseDir: './src',
+                name: 'Spa',
+                project: '',
+                out: 'dist/spa.d.ts'
+            },
+            build: {
+                src: ['src/**/*.ts']
             }
         }
     });
-    
+
     /* load every plugin in package.json */
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks("grunt-webpack");
+    grunt.loadNpmTasks('dts-generator');
     /* grunt tasks */
-    grunt.registerTask('build', ['typescript', 'uglify']);
+    grunt.registerTask('build', ['webpack', 'dtsGenerator']);
 
 };
