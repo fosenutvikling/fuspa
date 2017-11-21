5e39dcdf0102557ad4b09f0399898a4bf8cf33c3
Jonas Kirkemyr <jonas.kirkemyr@em1.no>
2017-11-21 20:39:47 +0100

    Refactor RouteMapper to singleton

--- src/RouteMapper.ts
+++ src/RouteMapper.ts
@@ -2,17 +2,16 @@ import * as hasher from 'hasher';
 import * as UrlPattern from 'url-pattern';
 
 export class RouteMapper {
-
     private static _instance: RouteMapper;
 
     private _runOnNextChange: Function;
     private _runOnChange: Function;
 
-    private routes: { pattern: UrlPattern, fn: Function }[];
+    private routes: { pattern: UrlPattern; fn: Function }[];
+    private _initialized: boolean;
 
     public static get instance(): RouteMapper {
-        if (this._instance == null)
-            this._instance = new RouteMapper();
+        if (this._instance == null) this._instance = new RouteMapper();
         return this._instance;
     }
 
@@ -20,9 +19,17 @@ export class RouteMapper {
         this._runOnChange = null;
         this._runOnNextChange = null;
         this.routes = [];
+        this._initialized = false;
     }
 
-    private initialize() {
+    public listen() {
+        if (this._initialized == false) {
+            this._initialized = true;
+            this.setupListeners();
+        } else throw Error('RouteMapper already initialized');
+    }
+
+    private setupListeners() {
         //add hash change listener
         hasher.changed.add(this.onHashChange);
 
@@ -37,21 +44,39 @@ export class RouteMapper {
             this._runOnNextChange = null;
         }
 
-        if (this._runOnChange !== null)
-            this._runOnChange();
+        if (this._runOnChange !== null) this._runOnChange();
 
         this.parse(newHash);
+        // else: emit event (to render notfound page)
     }
 
     private parse(check: string) {
         for (let i = 0; i < this.routes.length; ++i) {
             let matched = this.routes[i].pattern.match(check);
-            if (matched == null) {
-                this.routes[i].fn.apply(this, )
+            if (matched != null) {
+                let parameters = this.createCallbackParameterArray(matched);
+                this.routes[i].fn.apply(this, parameters);
                 return matched;
             }
         }
         return false;
+    }
+
+    private createCallbackParameterArray(parsedData: any[] | Object | any) {
+        let parameterArray = [];
+
+        if (parsedData instanceof Array) {
+            parameterArray = parsedData;
+        } else if (parsedData instanceof Object) {
+            // Important to clean the local data before appending new, as the last request has its data stored here already
+            Object.keys(parsedData).forEach(key => {
+                if (key !== '')
+                    // Don't want to append an empty key to the data array
+                    parameterArray.push(parsedData[key]);
+            });
+        } else throw new Error('Unknown type of parsedData data, expected Array || Object, got ' + typeof parsedData); // If parsedData variable is neither of an Array or Object, it's not recognized, and don't know how to handle it
+
+        return parameterArray;
     }
 
     public set runOnNextChange(fn: Function) {
