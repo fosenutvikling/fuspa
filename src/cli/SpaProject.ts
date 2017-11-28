efa66312450ed14a92d041c8e6eb620f334a3974
Jonas Kirkemyr <jonas.kirkemyr@em1.no>
2017-11-28 15:49:31 +0100

    Refactor: Move cli-specific code to own file

--- src/cli/SpaProject.ts
+++ src/cli/SpaProject.ts
@@ -1,5 +1,3 @@
-require('source-map-support').install()
-
 import { ProjectFiles } from './ProjectFiles/ProjectFiles';
 import * as npm from 'global-npm';
 import * as npmAddScript from 'npm-add-script';
@@ -9,21 +7,20 @@ import { envSlash } from './Functions';
 import * as fs from 'fs';
 import { iEngine } from './Engines/iEngine';
 
-interface IConfig {
+export interface IConfig {
     engine: 'handlebars',
     container: string,
     name: string,
     mainFile: string
 }
 
-class SpaProject {
+export class SpaProject {
     private engine: iEngine;
     private projectFiles: ProjectFiles;
     private config: IConfig;
 
     constructor(config: IConfig) {
         this.config = config;
-        console.log("Current config: ", this.config);
         this.verifyConfig();
         this.loadScript();
     }
@@ -55,7 +52,7 @@ class SpaProject {
 
     public create() {
         this.runNpm();
-        this.projectFiles.write();
+        //this.projectFiles.write();
     }
 
     private runNpm() {
@@ -92,43 +89,4 @@ class SpaProject {
     }
 }
 
-let configFilePath = process.cwd() + envSlash() + 'spa.options.js';
-
-let config: IConfig = {
-    engine: 'handlebars',
-    container: 'container',
-    name: 'spa-starter',
-    mainFile: 'main.ts'
-};
-
-try {
-    const loadedConfig: IConfig = require(configFilePath);
-
-    // Load default values for keys not set in required config
-    config.engine = loadedConfig.engine || config.engine;
-    config.container = loadedConfig.container || config.container;
-    config.name = loadedConfig.name || config.name;
-    config.mainFile = loadedConfig.mainFile || config.mainFile;
-}
-catch (exception) {
-    console.error('Couldn\'t locate spa.options.js, using default config');
-}
-
-const project = new SpaProject(config);
-//project.create();
-
-//var program = require('commander');
-
-//program
-//    .version('0.1.0')
-//    .option('-N, --obs <obse>', 'Name of project')
-//    .option('-f, --file <path>', 'set config path. defaults to ./spa.options.js', './spa.options.js')
-//    .command('<namert>')
-//    .parse(process.argv);
-
-//program.parse(process.argv);
-/*if (program.obs)
-    console.log("program.name", program.obs);
-console.log("NAMERT", program.namert);
-*/
-//if (program.namert)
+//let configFilePath = process.cwd() + envSlash() + 'spa.options.js';
