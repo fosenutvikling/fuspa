959735d05e21168c0ac9dddb56babe22b9048cae
Jonas Kirkemyr <jonas.kirkemyr@em1.no>
2017-12-03 16:53:15 +0100

    Chore: Update ts compilation process
    Decrease built time with multi-threaded building

--- src/cli/ProjectFiles/tasks/wepackJs.ts
+++ src/cli/ProjectFiles/tasks/wepackJs.ts
@@ -1,33 +1,44 @@
-// TODO: main file and project folder as input parameter. output folder and name
-// TODO: install dependencies required (add to npm install script)
-
 export const webpackJs = (mainFile, output) =>
   `
+var ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+  
 module.exports = {
   entry: '${mainFile}',
   output: {
     filename: '${output}'
   },
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000
+  },
   devtool: 'source-map',
   module: {
     rules: [{
       test: /\.tsx?$/,
-      loader: 'ts-loader',
-      exclude: /node_modules/,
-    }, {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-    },
-
-    {
-      test: /\.svg$/,
-      loader: 'svg-inline-loader'
-    }
+      use: [
+        { loader: 'cache-loader' },
+        {
+          loader: 'thread-loader',
+          options: {
+          // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+          workers: require('os').cpus().length - 1,
+        },
+      },
+      {
+        loader: 'ts-loader',
+        options: {
+          happyPackMode: true // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
+        }
+      }
     ]
+    }]
   },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  }
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({ checkSyntacticErrors: true }),
+    new UglifyJsPlugin({
+      sourceMap: true
+    })
+  ]
 };
 `;
